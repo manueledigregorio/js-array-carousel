1619775862e348e50e18ede52a9d9c139d1fe25b
manueledigregorio <dig.manuele@gmail.com>
2023-09-13 17:47:34 +0200

    [add] const java

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const itemsWrapper = document.querySelector('.items-wrapper')
+const btTop = document.querySelector('.bt-top')
+const btBottom = document.querySelector('.bt-bottom') 
+
+const image = [
+'img/01.webp',
+'img/02.webp',
+'img/03.webp',
+'img/04.webp',
+'img/05.webp'
+];
+
+for(let i=0; i<image.length;c++){
+  const img = image[i];
+
+
+
+}
+
+
+
